795c930785ba660fa27b031d0dbc91af32a35f5a
srushapi1 <98853671+srushapi1@users.noreply.github.com>
2022-03-23 22:31:12 -0400

    Create round-empty.php

---
+++ round-empty.php
@@ -0,0 +1,130 @@
+<?php return [
+	'pointScale' => 200,
+	'categories' => [
+		[
+			'name' => 'Animel',
+			'questions' =>  [
+				[
+				 	'question' => 'Gator?',
+				 	'answer'   => 'yes',
+				],
+				[
+				 	'question' => 'Tiger?',
+				 	'answer'   => 'yes',
+				],
+				[
+				 	'question' => 'Lion?',
+				 	'answer'   => 'yes',
+				],
+				[
+				 	'question' => 'Deer?',
+				 	'answer'   => 'no',
+				],
+				[
+				 	'question' => 'Cow?',
+				 	'answer'   => 'no',
+				],
+			]
+		],
+		[
+			'name' => 'Soccer',
+			'questions' =>  [
+				[
+				 	'question' => 'Which player is allowed to use their hands during the game?',
+				 	'answer'   => 'GK',
+				],
+				[
+				 	'question' => 'How many players are on the field for each team in an adult league if playing a full squad game?',
+				 	'answer'   => '11',
+				],
+				[
+				 	'question' => 'If a player is sent off, what color card does the referee display?',
+				 	'answer'   => 'Red',
+				],
+				[
+				 	'question' => 'How long is an official professionally sanctioned game?',
+				 	'answer'   => '90 Mins',
+				],
+				[
+				 	'question' => 'What color jerseys did the United States have the Women National team wear in 2007?',
+				 	'answer'   => 'Golden',
+				],
+			]
+		],
+		[
+			'name' => 'US History',
+			'questions' =>  [
+				[
+				 	'question' => 'What city was the first capital of the United States?',
+				 	'answer'   => 'Washington DC',
+				],
+				[
+				 	'question' => 'When was the Declaration of Independence signed?',
+				 	'answer'   => 'August 2nd, 1776 ',
+				],
+				[
+				 	'question' => 'What was the name of the treaty that ended the Revolutionary war? ',
+				 	'answer'   => 'Treaty of paris',
+				],
+				[
+				 	'question' => 'When did the Civil War end?',
+				 	'answer'   => '1895',
+				],
+				[
+				 	'question' => 'How many Americans were killed in World War II?',
+				 	'answer'   => '1.3 million',
+				],
+			]
+		],
+		[
+			'name' => 'Stormy Weather',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+		[
+			'name' => 'Tornado Safety and Trivia',
+			'questions' =>  [
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+				[
+				 	'question' => 'xx',
+				 	'answer'   => 'xx',
+				],
+			]
+		],
+	]
+];
