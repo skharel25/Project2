32c91d98a8c6747f65e73f49c94b4b8a81aa5adb
skharel25 <50557760+skharel25@users.noreply.github.com>
2022-03-25 13:40:48 -0400

    Update round-empty.php

--- round-empty.php
+++ round-empty.php
@@ -2,27 +2,27 @@
 	'pointScale' => 200,
 	'categories' => [
 		[
-			'name' => 'Animel',
+			'name' => 'Animal',
 			'questions' =>  [
 				[
-				 	'question' => 'Gator?',
-				 	'answer'   => 'yes',
+					'question' => 'How long is an elephant pregnant before it gives birth?',
+					'answer'   => '22 Months',
 				],
 				[
-				 	'question' => 'Tiger?',
-				 	'answer'   => 'yes',
+					'question' => 'What is the name of the fastest land animal?',
+					'answer'   => 'Cheetah',
 				],
 				[
-				 	'question' => 'Lion?',
-				 	'answer'   => 'yes',
+					'question' => 'What is the only mammal that can’t jump?',
+					'answer'   => 'Elephant',
 				],
 				[
-				 	'question' => 'Deer?',
-				 	'answer'   => 'no',
+					'question' => 'What animal is the largest mammal in the world?',
+					'answer'   => 'Blue Whale',
 				],
 				[
-				 	'question' => 'Cow?',
-				 	'answer'   => 'no',
+					'question' => 'Which animal can stand on its tail?',
+					'answer'   => 'Kangaroo',
 				],
 			]
 		],
@@ -30,24 +30,24 @@
 			'name' => 'Soccer',
 			'questions' =>  [
 				[
-				 	'question' => 'Which player is allowed to use their hands during the game?',
-				 	'answer'   => 'GK',
+					'question' => 'Which player is allowed to use their hands during the game?',
+					'answer'   => 'GK',
 				],
 				[
-				 	'question' => 'How many players are on the field for each team in an adult league if playing a full squad game?',
-				 	'answer'   => '11',
+					'question' => 'How many players are on the field for each team in an adult league if playing a full squad game?',
+					'answer'   => '11',
 				],
 				[
-				 	'question' => 'If a player is sent off, what color card does the referee display?',
-				 	'answer'   => 'Red',
+					'question' => 'If a player is sent off, what color card does the referee display?',
+					'answer'   => 'Red',
 				],
 				[
-				 	'question' => 'How long is an official professionally sanctioned game?',
-				 	'answer'   => '90 Mins',
+					'question' => 'How long is an official professionally sanctioned game?',
+					'answer'   => '90 Mins',
 				],
 				[
-				 	'question' => 'What color jerseys did the United States have the Women National team wear in 2007?',
-				 	'answer'   => 'Golden',
+					'question' => 'What color jerseys did the United States have the Women National team wear in 2007?',
+					'answer'   => 'Golden',
 				],
 			]
 		],
@@ -55,24 +55,24 @@
 			'name' => 'US History',
 			'questions' =>  [
 				[
-				 	'question' => 'What city was the first capital of the United States?',
-				 	'answer'   => 'Washington DC',
+					'question' => 'What city was the first capital of the United States?',
+					'answer'   => 'Washington DC',
 				],
 				[
-				 	'question' => 'When was the Declaration of Independence signed?',
-				 	'answer'   => 'August 2nd, 1776 ',
+					'question' => 'When was the Declaration of Independence signed?',
+					'answer'   => 'August 2nd, 1776 ',
 				],
 				[
-				 	'question' => 'What was the name of the treaty that ended the Revolutionary war? ',
-				 	'answer'   => 'Treaty of paris',
+					'question' => 'What was the name of the treaty that ended the Revolutionary war? ',
+					'answer'   => 'Treaty of paris',
 				],
 				[
-				 	'question' => 'When did the Civil War end?',
-				 	'answer'   => '1895',
+					'question' => 'When did the Civil War end?',
+					'answer'   => '1895',
 				],
 				[
-				 	'question' => 'How many Americans were killed in World War II?',
-				 	'answer'   => '1.3 million',
+					'question' => 'How many Americans were killed in World War II?',
+					'answer'   => '1.3 million',
 				],
 			]
 		],
@@ -80,24 +80,24 @@
 			'name' => 'Stormy Weather',
 			'questions' =>  [
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 			]
 		],
@@ -105,24 +105,24 @@
 			'name' => 'Tornado Safety and Trivia',
 			'questions' =>  [
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 				[
-				 	'question' => 'xx',
-				 	'answer'   => 'xx',
+					'question' => 'xx',
+					'answer'   => 'xx',
 				],
 			]
 		],
